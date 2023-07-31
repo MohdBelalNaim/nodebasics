10fcb1e72211841f69682b33868dae6282f6be72
Belal <contactbelalnaim@gmail.com>
2023-07-31 11:52:01 +0530

    Added routes

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 
 
-app.listen(5000, ()=>{
-    console.log("App is running on port 5000")
+app.use(express.json())
+
+app.post("/login",(req,res)=>{
+    //sending json data
+    const {one,two,three} = req.body
+
+    if(!email || !password){
+        res.json({error:"All fields are required"})
+    }
+    else{
+        res.json({success:"Data recieved"})
+    }
 })
+
+app.post("/",(req,res)=>{
+    res.json({message:"This is a json data"})
+})
+
+
+app.listen(5000, ()=>{console.log("App is running on port 5000")})
